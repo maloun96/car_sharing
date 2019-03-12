fa4423892e091e27f208e0386b1a7a7b76501f00
victor <malai9696@gmail.com>
2019-03-12 12:31:40 +0200

    Add migratin

---
+++ database/migrations/2019_03_12_091655_create_parking_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParkingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parking', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->tinyInteger('status');
+			$table->integer('capacity');
+			$table->integer('price');
+			$table->string('latitude');
+			$table->string('longitude');
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parking');
+    }
+}
